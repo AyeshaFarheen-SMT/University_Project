a9d2aad202c0cbb0a3f67d6d3c20217baeffe42b
AyeshaFarheen-SMT <AYESHA.20201CSE0588@presidencyuniversity.in>
2023-12-22 21:44:57 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+
+// var typed = new Typed( '#mytext',{
+//         strings : ['Power Your TMS With Intelligent Routing'],
+//         typeSpeed : 170
+//     }
+// )
+
+// var typed = new Typed( '#myabstract',{
+//     strings : [''],
+//     typeSpeed : 1002
+// }
+// )
+
+AOS.init(
+    {
+        duration : 1000
+    }
+)
